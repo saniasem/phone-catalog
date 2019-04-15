f323352dcf2b585da65578d51c6f7b9f89d93869
Oleksandr Semeniuk <olexandr.semenyuk@hotmail.com>
2019-04-15 15:57:41 +0300

    Gallery implemented

--- js/phones/components/phone-viewer.js
+++ js/phones/components/phone-viewer.js
@@ -6,6 +6,12 @@ export default class PhoneViewer extends Component {
     this.onBack = onBack;
 
     this.on("click", '[data-element="btn-element"]', this.onBack);
+    this.on("click", '[data-element="small-preview"]', event => {
+      const bigPreview = this._element.querySelector(
+        '[data-element="big-preview"]'
+      );
+      bigPreview.src = event.target.src;
+    });
   }
 
   show(phoneDetails) {
@@ -16,7 +22,9 @@ export default class PhoneViewer extends Component {
 
   _render() {
     this._element.innerHTML = `
-      <img class="phone" src="${this._phoneDetails.images[0]}">
+      <img class="phone" src="${
+        this._phoneDetails.images[0]
+      }" data-element="big-preview">
 
       <button data-element="btn-element">Back</button>
       <button>Add to basket</button>
@@ -27,24 +35,16 @@ export default class PhoneViewer extends Component {
       <p>Motorola XOOM with Wi-Fi has a super-powerful dual-core processor and Android™ 3.0 (Honeycomb) — the Android platform designed specifically for tablets. With its 10.1-inch HD widescreen display, you’ll enjoy HD video in a thin, light, powerful and upgradeable tablet.</p>
 
       <ul class="phone-thumbs">
-        <li>
-            <img src="img/phones/motorola-xoom-with-wi-fi.0.jpg">
-        </li>
-        <li>
-            <img src="img/phones/motorola-xoom-with-wi-fi.1.jpg">
-        </li>
-        <li>
-            <img src="img/phones/motorola-xoom-with-wi-fi.2.jpg">
-        </li>
-        <li>
-            <img src="img/phones/motorola-xoom-with-wi-fi.3.jpg">
-        </li>
-        <li>
-            <img src="img/phones/motorola-xoom-with-wi-fi.4.jpg">
-        </li>
-        <li>
-            <img src="img/phones/motorola-xoom-with-wi-fi.5.jpg">
-        </li>
+        ${this._phoneDetails.images
+          .map(
+            imageUrl =>
+              (this._element.innerHTML = `
+          <li>
+            <img src="${imageUrl}" data-element="small-preview">
+          </li> 
+          `)
+          )
+          .join("")}
       </ul>
       `;
   }
